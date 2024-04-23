d320f013ad378b9f665fa6b2c91b7aa57c3c4317
bittar <rafabittarg@gmail.com>
2024-04-23 16:53:30 -0300

    teste boy

--- teste.py
+++ teste.py
@@ -29,8 +29,8 @@ while running:
         player_pos.y += 300 * dt
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    
+        #se fu
 
     # flip() the display to put your work on screen
     pygame.display.flip()
