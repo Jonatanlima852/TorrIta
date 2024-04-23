dc6faefb99034604aa032ab9f3633c3c018e24a9
Jonatan de Lima <jonatanlima852@gmail.com>
2024-04-23 16:57:06 -0300

    teste git bibs

--- teste.py
+++ teste.py
@@ -20,6 +20,7 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
+    #teste de git
     pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
